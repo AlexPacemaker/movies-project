504f059c68883fa95760dd01b3f4c25c6a39100e
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-05-15 16:58:00 +0300

    Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,21 +5,30 @@ import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <div className={styles.navMaine}>
-      <nav className='#4db6ac teal lighten-2'>
+      <nav className='#1e88e5 blue darken-1'>
         <div className='nav-wrapper'>
           <div className={styles.refs}>
-            <a href='#s' className='brand-logo'>
-              Logo
+            <a href='#s' className={styles.brandLogo}>
+              React Movie DB
             </a>
             <ul id='nav-mobile' className='right hide-on-med-and-down'>
               <li>
-                <a href='sass.html'>Sass</a>
+                <a href='https://github.com/AlexPacemaker' target='blank'>
+                  GitHub
+                </a>
               </li>
               <li>
-                <a href='badges.html'>Components</a>
+                <a
+                  href='https://alexpacemaker.github.io/portfolio/'
+                  target='blank'
+                >
+                  Portfolio
+                </a>
               </li>
               <li>
-                <a href='collapsible.html'>JavaScript</a>
+                <a href='https://github.com/AlexPacemaker/movies-project'>
+                  Code Source
+                </a>
               </li>
             </ul>
           </div>
