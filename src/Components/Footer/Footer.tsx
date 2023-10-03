4237dbb2f94602677c0c603861019a8ed217751c
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-03 15:11:18 +0300

    upd

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,15 +2,13 @@
 import React from "react";
 import styles from "./Footer.module.scss";
 
-const Footer: React.FC = () => {
+const Footer = (): JSX.Element => {
   return (
     <div className={styles.footer}>
       <footer className='page-footer #1e88e5 blue darken-1'>
         <div className='container'></div>
         <div className='white-text footer-copyright'>
-          <div className='container'>
-            PaceMaker © {new Date().getFullYear()}
-          </div>
+          <div className='container'>PaceMaker © {new Date().getFullYear()}</div>
         </div>
       </footer>
     </div>
